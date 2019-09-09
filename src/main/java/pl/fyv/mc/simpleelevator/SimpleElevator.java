8d1a24f4f706c1ff8005c9d8a61d8c4fabeebb69
fyv <grzegorz.nowak.formal@gmail.com>
2019-09-09 20:12:27 +0200

    - added specified tag for plugin sign: [Elevator] - amount of Y levels is now in 4th line - improved code

--- src/main/java/pl/fyv/mc/simpleelevator/SimpleElevator.java
+++ src/main/java/pl/fyv/mc/simpleelevator/SimpleElevator.java
@@ -13,8 +13,12 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public class SimpleElevator extends JavaPlugin implements Listener {
 
+
+    private static final int SIGN_LINE = 3;
     @Override
     public void onEnable() {
         getLogger().info("Simple Elevator: onEnable is called");
@@ -29,7 +33,6 @@ public class SimpleElevator extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-
         if(cmd.getName().equalsIgnoreCase("simpleelevator")) {
             sender.sendMessage("(c) fyv 2019");
             return true;
@@ -43,22 +46,32 @@ public class SimpleElevator extends JavaPlugin implements Listener {
         Player player = e.getPlayer();
         if(e.getAction() == Action.RIGHT_CLICK_BLOCK) {
 
-            Block block = e.getClickedBlock();
-             if(block.getType() == Material.ACACIA_WALL_SIGN ||
-                block.getType() == Material.BIRCH_WALL_SIGN ||
-                block.getType() == Material.DARK_OAK_WALL_SIGN ||
-                block.getType() == Material.JUNGLE_WALL_SIGN ||
-                block.getType() == Material.OAK_WALL_SIGN ||
-                block.getType() == Material.SPRUCE_WALL_SIGN ) {
 
-                Sign sign = (Sign)block.getState();
+            if(e.getClickedBlock()!=null) {
 
-                double newy = Double.parseDouble(sign.getLine(2));
+                Block block = e.getClickedBlock();
+                if (block.getType() == Material.ACACIA_WALL_SIGN ||
+                    block.getType() == Material.BIRCH_WALL_SIGN ||
+                    block.getType() == Material.DARK_OAK_WALL_SIGN ||
+                    block.getType() == Material.JUNGLE_WALL_SIGN ||
+                    block.getType() == Material.OAK_WALL_SIGN ||
+                    block.getType() == Material.SPRUCE_WALL_SIGN ) {
 
-                Location l = player.getLocation();
 
-                player.teleport(new Location(player.getWorld(), l.getX(), l.getY()+newy, l.getZ(), l.getYaw(), l.getPitch()));
+                    Sign sign = (Sign)block.getState();
+                    if(sign.getLine(0).equalsIgnoreCase("[Elevator]")) {
+                        if (sign.getLine(SIGN_LINE).length() > 0)
+                            try {
 
+                                double newy = Double.parseDouble(sign.getLine(SIGN_LINE));
+                                Location l = player.getLocation();
+
+                                player.teleport(new Location(player.getWorld(), l.getX(), l.getY() + newy, l.getZ(), l.getYaw(), l.getPitch()));
+                            } catch (Exception ex) {
+                                player.sendMessage("4th line is not a number! Create new sign with number in 4th line");
+                            }
+                    }
+                }
             }
         }
     }
